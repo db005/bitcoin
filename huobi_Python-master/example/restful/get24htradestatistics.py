a7d8abadfca9a16f24249f7b039d1f22d4d4528e
db005 <297347023@qq.com>
2020-03-03 13:00:42 +0800

    删除火币

--- huobi_Python-master/example/restful/get24htradestatistics.py
+++
@@ -1,9 +0,0 @@
-from huobi import RequestClient
-
-request_client = RequestClient()
-
-symbol_list = ["btcusdt", "ethusdt", "eosusdt", "htusdt"]
-for symbol_row in symbol_list:
-    trade_statistics = request_client.get_24h_trade_statistics(symbol_row)
-    print("---- Statistics ----")
-    trade_statistics.print_object()
--- huobi_Python-master/huobi/impl/utils/timeservice.py
+++
@@ -1,7 +0,0 @@
-import time
-
-
-def get_current_timestamp():
-    return int(round(time.time() * 1000))
-
-
